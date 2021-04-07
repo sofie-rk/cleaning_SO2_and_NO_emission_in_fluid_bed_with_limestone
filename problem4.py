4da04b9c7191fae405d73b802c58ae9182dccd9b
sofie-rk <sofierk@stud.ntnu.no>
2021-04-07 15:11:23 +0200

    .

--- problem4.py
+++ problem4.py
@@ -28,6 +28,12 @@ def outlet_concentrations(T, beta, y_NO_in):
 
     y_NH3_out = (-b + (b**2 - 4*a*c)**(1/2))/(2*a)
 
+    y_NH3_out1 = (-b + (b**2 - 4*a*c)**(1/2))/(2*a)
+    y_NH3_out2 = (-b - (b**2 - 4*a*c)**(1/2))/(2*a)
+
+    if (y_NH3_out1 > 0 and y_NH3_out2 > 0):
+        print("!!!")
+
     if (y_NH3_out < 0):
         y_NH3_out = (-b - (b**2 - 4*a*c)**(1/2))/(2*a)
     
@@ -58,11 +64,10 @@ for beta in beta_list:
 
     for j in range(T_points):
         NO_conc_list[j], NH3_conc_list[j] = outlet_concentrations(T_list[j] + 273, beta, y_NO_in)
-
-
     
     NO_ax1.plot(T_list, NO_conc_list, label=r"$\beta$ = " + str(beta))
     NH3_ax1.plot(T_list, NH3_conc_list, label=r"$\beta$ = " + str(beta))
+
 
 NO_ax1.set(xlabel=xlabel_str1, ylabel="NO concentration [ppmv]")
 NH3_ax1.set(xlabel=xlabel_str1, ylabel="NH3 concentration [ppmv]")
@@ -78,7 +83,7 @@ plt.show()
 
 fig2, (NO_ax2, NH3_ax2) = plt.subplots(1, 2)
 
-temperatures = [750, 900, 1000, 1200]
+temperatures = [750, 800, 1000, 1200] # [C]
 
 for T in temperatures:
 
@@ -93,7 +98,7 @@ for T in temperatures:
     for j in range(y_NO_in_points):
         NO_conc_list[j], NH3_conc_list[j] = outlet_concentrations(T+273, beta, y_NO_in_list[j])
 
-
+    print("T: ", T, " NO: ", NO_conc_list[-1])
     NO_ax2.plot(y_NO_in_list, NO_conc_list, label="T = " + str(T) + " $^o$C")
     NH3_ax2.plot(y_NO_in_list, NH3_conc_list, label="T = " + str(T) + " $^o$C")
 
